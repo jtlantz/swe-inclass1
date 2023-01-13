59037c569dc2313b168072ae972e84e526bb9cf1
justyn lantz <justyn@DESKTOP-7JDRPAI.localdomain>
2023-01-13 16:31:12 +0700

    add the combined method, move tests to test folder

--- stats.py
+++ stats.py
@@ -22,17 +22,35 @@ def compute(file):
         arr = [float(line) for line in f]
     #convert to dataframe
     df = pd.DataFrame(arr)
-    print(f"mean: {df.mean().values[0]}")
-    print(f"std: {df.std().values[0]}")
-    print(f"min: {df.min().values[0]}")
-    print(f"max: {df.max().values[0]}")
+    print(file)
+    print_stats(df)
+
+def compute_all(files):
+    #read each line in file
+    arr = []
+    for file in files:
+        with open(file, 'r') as f:
+            arr += [float(line) for line in f]
+    #convert to dataframe
+    df = pd.DataFrame(arr)
+    print("Combined")
+    print_stats(df)
+
+def print_stats(arr: pd.DataFrame):
+    print(f"mean: {arr.mean().values[0]}")
+    print(f"std: {arr.std().values[0]}")
+    print(f"min: {arr.min().values[0]}")
+    print(f"max: {arr.max().values[0]}")
 
 if __name__ == "__main__":
-    #take the first name
+    #get all names of files
     files = sys.argv[1:]
     #by filtering this way, the whole program won't crash from one bad txt file
+    valid_files = []
     for fn in files:
         if verify_input(fn):
-            compute(fn) 
+            compute(fn)
+            valid_files.append(fn)
         else:
             print(f"File {fn} contains invalid data.")
+    compute_all(valid_files)
